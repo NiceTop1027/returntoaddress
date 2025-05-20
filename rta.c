c81b7a9a04b77da134a729778b38a72976ad8f81
나이스탑 <mistarcodm@gmail.com>
2025-05-21 02:15:09 +0900

    Add files via upload

--- rta.c
+++ rta.c
@@ -17,17 +17,7 @@ void win(){
 int main(){
     init();
     char buf[0x100];
-    int a = 30;
-    int b = 100;
-
     printf("input : ");
     scanf("%s", buf);
-
-    if(a != 30 && b != 100){
-        printf("오호라..");   
-    }
-    else{
-        exit(0);
-    }
     return 0;
 }
